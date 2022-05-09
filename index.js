08ff47b0b7309fe794b5051d824219df111794a6
Radoslaw Palka <radekpalka@gmail..com>
2022-05-09 20:08:09 +0200

    create partials folder with header.ejs and footer.ejs

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', __dirname+ '/views')
 app.get('/', (req, res) =>{
-  res.render('homepage')
+  res.render('homepage',  {link: "/test"})
+})
+app.get('/test', (req, res) =>{
+  res.render('test')
 })
 
 
